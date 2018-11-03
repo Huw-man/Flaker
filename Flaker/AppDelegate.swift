c72d42e9877725c4719c40040ad6dcb0f0b70f9e
Huw-man <hu.newman@berkeley.edu>
2018-11-03 15:37:23 -0700

    added location tracking

--- Flaker/AppDelegate.swift
+++ Flaker/AppDelegate.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
+import UserNotifications    
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    //instance variables
+    let center = UNUserNotificationCenter.current()
+    let locationManager = CLLocationManager()
+    static let geoCoder = CLGeocoder()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+        }
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+        
+        // Set a movement threshold for new events.
+        locationManager.distanceFilter = 15; // meters
+        locationManager.startMonitoringVisits()
+        locationManager.delegate = self
         return true
     }
 
@@ -40,7 +57,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+}
 
-
+extension AppDelegate: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+        // create CLLocation from the coordinates of CLVisit
+        let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
+        
+        // Get location description
+        AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
+            if let place = placemarks?.first {
+                let description = "\(place)"
+                self.newVisitReceived(visit, description: description)
+            }
+        }
+    }
+    
+    func newVisitReceived(_ visit: CLVisit, description: String) {
+        let location = Location(visit: visit, descriptionString: description)
+        // 1
+        let content = UNMutableNotificationContent()
+        content.title = "New Journal entry 📌"
+        content.body = location.description
+        content.sound = .default
+        
+        // 2
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+        
+        // 3
+        center.add(request, withCompletionHandler: nil)
+        // Save location to disk
+    }
+    
+    
 }
 

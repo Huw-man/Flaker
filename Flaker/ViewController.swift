1618ca375825d95766754d635295c2eab8abefa6
Huw-man <hu.newman@berkeley.edu>
2018-11-03 13:19:52 -0700

    Initial Commit

---
+++ Flaker/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Flaker
+//
+//  Created by Newman Hu on 11/3/18.
+//  Copyright © 2018 Newman Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var newCodeLabel: UITextField!
+    @IBOutlet weak var joinCodeLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // handle the code fields input through delegate callbacks
+        newCodeLabel.delegate = self
+    }
+    
+    //MARK: Actions
+    @IBAction func createParty(_ sender: UIButton) {
+    }
+    
+    @IBAction func joinParty(_ sender: Any) {
+    }
+    
+
+}
+

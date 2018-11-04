f287b11a0804ea9386932f04746208a25c6a99e1
Mac User <macuser@calvisitor-10-105-185-21.calvisitor.1918.berkeley.edu>
2018-11-03 18:45:07 -0700

    woojins mom

--- Flaker/ViewController.swift
+++ Flaker/ViewController.swift
@@ -18,9 +18,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // handle the code fields input through delegate callbacks
         joinCodeLabel.delegate = self
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLength = 4
+        let currentString: NSString = textField.text! as NSString
+        let newString: NSString =
+            currentString.replacingCharacters(in: range, with: string) as NSString
+        return newString.length <= maxLength
     }
 
     //MARK: UITextFieldDelegate
